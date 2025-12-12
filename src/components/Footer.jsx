574fc76c6083dfd90adab1a7f77cc4841267ddfa
Abrahan Tolentino <abrahantv@icloud.com>
2025-12-11 23:32:37 -0500

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
     <>
-      <footer>
-        <div className="container-fluid d-flex align-items-center p-4 mt-4 bg-primary">
-          <p className="text-danger">tuki tuki tuki tuki</p>
+      <footer id="footer">
+        <div className="container-fluid d-flex align-items-center justify-content-center p-4 mt-4 bg-primary">
+          <p className="text-danger fs-1 fw-bold">THIS IS A WORK IN PROGRESS</p>
         </div>
       </footer>
     </>
